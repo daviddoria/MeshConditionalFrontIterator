4173ab9535b7b6aa3c8e4bce3d81b28085a463f8
David Doria <daviddoria@gmail.com>
2011-07-26 13:55:44 -0400

    Removed unnecessary files - oops

--- Article/bin/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/portable/VTKJournalSubmissions/vtkMeshConditionalFrontIterator/source
-# Build directory: /media/portable/VTKJournalSubmissions/vtkMeshConditionalFrontIterator/bin
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(vtkMeshConditionalFrontIteratorTest "vtkMeshConditionalFrontIteratorTest")
--- Article/source/vtkMeshConditionalFrontIterator/build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /media/portable/VTK_Additions/vtkMeshConditionalFrontIterator
-# Build directory: /media/portable/VTK_Additions/vtkMeshConditionalFrontIterator/build
-# 
-# This file includes the relevent testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-ADD_TEST(vtkMeshConditionalFrontIteratorTest "vtkMeshConditionalFrontIteratorTest")
